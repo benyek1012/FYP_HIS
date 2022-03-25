07bac9bdfb48ff9ec24c4e2fee1a14270a787391
xueqian666 <zhangxueqian666@gmail.com>
2022-03-26 02:12:20 +0800

    ver 1

---
+++ HIS/views/layouts/sidebar.php
@@ -0,0 +1,91 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="HIS Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">HIS</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Alexander Pierce</a>
+            </div>
+        </div> -->
+
+        <!-- Search Bar -->
+        <form class="form-inline ml-3">
+            <div class="input-group input-group-sm">
+                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-navbar" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        <!-- SidebarSearch Form -->
+        <!-- href be escaped -->
+        <!-- <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div>
+            </div>
+        </div> -->
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <?php
+            echo \hail812\adminlte\widgets\Menu::widget([
+                'items' => [
+                    // ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
+                    ['label' => 'Patient NRIC', 'header' => true],
+                    // ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
+                    // ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
+                    // ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
+                    ['label' => 'Patient Name 1', 'url' => ['/debug'], 'target' => '_blank'],
+                    ['label' => 'Patient Name 2', 'url' => ['/debug'], 'target' => '_blank'],
+                    ['label' => 'R/N Number', 'header' => true],
+                    ['label' => '1'],
+                    ['label' => '2'],
+                    ['label' => '3'],
+                    // [
+                    //     'label' => 'Level1',
+                    //     'items' => [
+                    //         ['label' => 'Level2', 'iconStyle' => 'far'],
+                    //         [
+                    //             'label' => 'Level2',
+                    //             'iconStyle' => 'far',
+                    //             'items' => [
+                    //                 ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+                    //                 ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+                    //                 ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle']
+                    //             ]
+                    //         ],
+                    //         ['label' => 'Level2', 'iconStyle' => 'far']
+                    //     ]
+                    // ],
+                    // ['label' => 'Level1'],
+                    ['label' => '', 'header' => true],
+                    ['label' => 'Admission', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
+                    ['label' => 'Deposit', 'iconClass' => 'nav-icon far fa-circle text-warning'],
+                    ['label' => 'Treatment Details', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
+                    ['label' => 'Bill', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
+                    ['label' => 'Payment', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
+                ],
+            ]);
+            ?>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
