73610e95b2ec3a25a4daa0a74ba3e183c6d687df
Benjamin Yek <102765903@students.swinburne.edu.my>
2022-05-20 12:10:59 +0800

    Update dbhis.sql
    General Lookup Codes  setup (Race)
    
    Give username of user (unique)

--- dbhis.sql
+++ dbhis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2022 at 04:18 PM
+-- Generation Time: May 20, 2022 at 06:07 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.7
 
@@ -120,19 +120,20 @@ INSERT INTO `lookup_general` (`lookup_general_uid`, `code`, `category`, `name`, 
 ('aaFXZAE9L9368Ha8kUCr4WtSXNPjsQGw', 'Indonesia', 'Nationality', 'Indonesia', 'Indonesia', 1),
 ('aRYQoZ0PUgME-uCF_cdxjF6dYp8eSMfk', 'Son', 'Relationship', 'Son', 'Son', 1),
 ('DCMaNq6WPc6L5tP-kYnPUFhZPJ_l-EKC', 'China', 'Nationality', 'China', 'China', 1),
-('Dh9GP_xVMCs1IS_6CKineglPl56QYVUv', 'Kadazandusun', 'Race', 'Kadazandusun', 'Kadazandusun', 1),
-('dysj6KiH3X4jsDxS3N3b3GlkNV9yJOPx', 'Chinese', 'Race', 'Chinese', 'Chinese', 1),
-('DyUydTXK7F2zYXrFjfANS06IlGhRnbQ3', 'Indian', 'Race', 'Indian', 'Indian', 1),
+('Dh9GP_xVMCs1IS_6CKineglPl56QYVUv', 'ME', 'Race', 'Melanau', 'Melanau', 1),
+('dysj6KiH3X4jsDxS3N3b3GlkNV9yJOPx', 'CH', 'Race', 'Cina', 'Cina', 1),
+('DyUydTXK7F2zYXrFjfANS06IlGhRnbQ3', 'BI', 'Race', 'Bidayuh', 'Bidayuh', 1),
 ('EnThnPoarmFJQ6LyKfBr0vTT5ChY4Vvi', 'Singapore', 'Nationality', 'Singapore', 'Singapore', 1),
 ('jmUi7zek4A-0EKslcAAUhJUVcplGa8wz', 'Husband/Spouse', 'Relationship', 'Husband/Spouse', 'Husband/Spouse', 1),
 ('Jor2cgaU0etz67yALq3odFYCl2n5Uc_E', 'Female', 'Sex', 'Female', 'Female', 1),
-('NH2m9rCS3gW1kml1GFtJQBd6W7KsDjMr', 'Iban', 'Race', 'Iban', 'Iban', 1),
+('NgRT9pY7CuuVhBgl2hzZE5o1uGFC3YX0', 'OI', 'Race', 'Others', 'Others (Kenyah, Kelabit, Lunbawang, Kayan, Penan etc)', 1),
+('NH2m9rCS3gW1kml1GFtJQBd6W7KsDjMr', 'IB', 'Race', 'Iban', 'Iban', 1),
 ('q18lvwYjLv8qE-FT9yYgQikmaekPGyoZ', 'Male', 'Sex', 'Male', 'Male', 1),
 ('SHxs6f-CqlgvJOACnEJ6jWpE6wl9aFJY', 'Thailand', 'Nationality', 'Thailand', 'Thailand', 1),
 ('smIfbSJJI1b970rWS0it1kCqDaRMsSSg', 'Malaysia', 'Nationality', 'Malaysia', 'Malaysia', 1),
 ('tpMLuKI7rr4jn0FVyCWA2c62DyOKsrYe', 'Brother', 'Relationship', 'Brother', 'Brother', 1),
 ('u5lcXSAV0bYqVclKtQZao9xRRTykmA_O', 'Sister', 'Relationship', 'Sister', 'Sister', 1),
-('UAaI0eNAqAEk6VwJaMqXGeYHF9rsqFWY', 'Malay', 'Race', 'Malay', 'Malay', 1),
+('UAaI0eNAqAEk6VwJaMqXGeYHF9rsqFWY', 'MA', 'Race', 'Malay', 'Malay', 1),
 ('WQSPmw1lcpysbN3sR_IpQsvvEQ8WFzBf', 'Daughter', 'Relationship', 'Daughter', 'Daughter', 1);
 
 -- --------------------------------------------------------
@@ -531,7 +532,9 @@ ALTER TABLE `lookup_ward`
 -- Indexes for table `new_user`
 --
 ALTER TABLE `new_user`
-  ADD PRIMARY KEY (`user_uid`);
+  ADD PRIMARY KEY (`user_uid`),
+  ADD UNIQUE KEY `user_uid` (`user_uid`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `patient_admission`
