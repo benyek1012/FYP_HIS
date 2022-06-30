dcbb5a2936b0af851332e4db8191a6a0a12af9a6
Benjamin Yek <102765903@students.swinburne.edu.my>
2022-06-30 22:13:44 +0800

    Update dbhis.sql
    Implementing two stored procedure for transaction records and payment

--- dbhis.sql
+++ dbhis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2022 at 03:59 PM
+-- Generation Time: Jun 30, 2022 at 03:34 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.1.6
 
@@ -22,6 +22,46 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `dbhis` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `dbhis`;
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `receipt_bill_procedure` (IN `rn` VARCHAR(11))   BEGIN
+
+   SELECT receipt.rn, receipt.receipt_content_datetime_paid, receipt.receipt_content_sum, receipt.receipt_type, 	receipt.receipt_content_payment_method, receipt.receipt_content_payer_name, receipt.receipt_serial_number, receipt.receipt_content_description, receipt.receipt_responsible FROM receipt WHERE receipt.rn = rn
+    UNION
+    SELECT bill.rn, bill.bill_generation_datetime, bill.bill_generation_billable_sum_rm, null, null, null, bill.bill_print_id, null, bill.generation_responsible_uid FROM bill WHERE bill.deleted = 0 AND bill.rn = rn AND bill.bill_generation_datetime != '';
+
+    
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `transaction_records` (IN `pid` VARCHAR(64))   BEGIN
+
+	SELECT receipt.rn, receipt.receipt_content_datetime_paid, receipt.receipt_content_sum, receipt.receipt_type, 	receipt.receipt_content_payment_method, receipt.receipt_content_payer_name, receipt.receipt_serial_number, receipt.receipt_content_description, receipt.receipt_responsible
+	FROM receipt
+	INNER JOIN patient_admission 
+    ON patient_admission.rn = receipt.rn
+    INNER JOIN patient_information
+    ON patient_information.patient_uid = patient_admission.patient_uid
+    WHERE patient_information.patient_uid = pid
+    
+    UNION
+     
+    SELECT bill.rn, bill.bill_generation_datetime, bill.bill_generation_billable_sum_rm, null, null, null, bill.bill_print_id, null, bill.generation_responsible_uid 
+    FROM bill 
+    INNER JOIN patient_admission 
+    ON patient_admission.rn = bill.rn
+    INNER JOIN patient_information
+    ON patient_information.patient_uid = patient_admission.patient_uid
+    WHERE patient_information.patient_uid = pid 
+    AND bill.deleted = 0 AND bill.bill_generation_datetime != '';
+
+    
+ 
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
