14894d331d6a2edcb7e1182e08221c1dfbbd632d
xueqian666 <zhangxueqian666@gmail.com>
2022-08-06 15:52:11 +0800

    Fixed Bug : Patient search by name page: Don't auto-show patient list

--- HISkartik/models/Patient_admissionSearch.php
+++ HISkartik/models/Patient_admissionSearch.php
@@ -5,6 +5,8 @@ namespace app\models;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\Patient_admission;
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
+use phpDocumentor\Reflection\Types\Null_;
 
 /**
  * Patient_admissionSearch represents the model behind the search form of `app\models\Patient_admission`.
@@ -43,7 +45,7 @@ class Patient_admissionSearch extends Patient_admission
      */
     public function search($params)
     {
-        
+        $this->load($params);
         $datetime = Patient_admission::find()
         ->select('MAX(entry_datetime)')
         ->from("patient_admission")
@@ -54,41 +56,54 @@ class Patient_admissionSearch extends Patient_admission
         ->from('patient_admission')
         ->joinWith('patient_information',true)
         ->where(['in','entry_datetime',$datetime])
+        // ->andWhere(['name' => $this->name])
         ->groupBy(['patient_uid']);
         
+
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
 
-        $this->load($params);
+        // echo "<pre>";
+        // var_dump($this->name);
+        // exit();
+        // echo "</pre>";
+        if(empty($this->name)){
 
+            $query->where(['name' => NULL]);
+        }
+        else{
+            $query->andFilterWhere(['like', 'name', $this->name]);
+        }
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
             // $query->where('0=1');
             return $dataProvider;
         }
+      
 
         // grid filtering conditions
-        $query->andFilterWhere([
-            'entry_datetime' => $this->entry_datetime,
-            'medical_legal_code' => $this->medical_legal_code,
-            'reminder_given' => $this->reminder_given,
-        ]);
+    
+            // $query->andFilterWhere([
+            //     'entry_datetime' => $this->entry_datetime,
+            //     'medical_legal_code' => $this->medical_legal_code,
+            //     'reminder_given' => $this->reminder_given,
+            // ]);
 
-        $query->andFilterWhere(['like', 'rn', $this->rn])
-            ->andFilterWhere(['like', 'patient_uid', $this->patient_uid])
-            ->andFilterWhere(['like', 'initial_ward_code', $this->initial_ward_code])
-            ->andFilterWhere(['like', 'initial_ward_class', $this->initial_ward_class])
-            ->andFilterWhere(['like', 'reference', $this->reference])
-            ->andFilterWhere(['like', 'guarantor_name', $this->guarantor_name])
-            ->andFilterWhere(['like', 'guarantor_nric', $this->guarantor_nric])
-            ->andFilterWhere(['like', 'guarantor_phone_number', $this->guarantor_phone_number])
-            ->andFilterWhere(['like', 'guarantor_email', $this->guarantor_email])
-            ->andFilterWhere(['like', 'type', $this->type])
-            ->andFilterWhere(['like', 'name', $this->name])
-            ->andFilterWhere(['like', 'nric', $this->nric])
-            ->andFilterWhere(['like', 'sex', $this->sex])
-            ->andFilterWhere(['like', 'race', $this->race]);
+            // $query->andFilterWhere(['like', 'rn', $this->rn])
+            //     ->andFilterWhere(['like', 'patient_uid', $this->patient_uid])
+            //     ->andFilterWhere(['like', 'initial_ward_code', $this->initial_ward_code])
+            //     ->andFilterWhere(['like', 'initial_ward_class', $this->initial_ward_class])
+            //     ->andFilterWhere(['like', 'reference', $this->reference])
+            //     ->andFilterWhere(['like', 'guarantor_name', $this->guarantor_name])
+            //     ->andFilterWhere(['like', 'guarantor_nric', $this->guarantor_nric])
+            //     ->andFilterWhere(['like', 'guarantor_phone_number', $this->guarantor_phone_number])
+            //     ->andFilterWhere(['like', 'guarantor_email', $this->guarantor_email])
+            //     ->andFilterWhere(['like', 'type', $this->type])
+            //     ->andFilterWhere(['like', 'name', $this->name])
+            //     ->andFilterWhere(['like', 'nric', $this->nric])
+            //     ->andFilterWhere(['like', 'sex', $this->sex])
+            //     ->andFilterWhere(['like', 'race', $this->race]);
         return $dataProvider;
     }
 }
--- HISkartik/views/patient_admission/search_index.php
+++ HISkartik/views/patient_admission/search_index.php
@@ -36,121 +36,130 @@ $model = Patient_information::findOne(Yii::$app->request->get('id'));
         </div>
         <!-- /.card-header -->
         <div class="card-body">
-            <!-- This is the gridview that shows patient admission summary-->
-            <?= kartik\grid\GridView::widget([
-                    'dataProvider' => $dataProvider,
-                // 'filterModel' => $searchModel,
-                    'showOnEmpty' => false,
-                    'hover' => true,
-                    'striped' => false,
-                    'condensed' => false,
-                    'emptyText' => Yii::t('app','Patient admission record is not founded'),
-                
-                    'rowOptions' => function($model) {
-                        // $url = Url::to([Yii::$app->controller->id.'/index', 'id' => $model['patient_uid']]);
-                        $urlPatientAdmission = Url::toRoute(['patient_admission/patient', 'id' => $model['patient_uid']]);
-                        $urlPatientInformation = Url::toRoute(['patient_information/patient', 'id' => $model['patient_uid']]);
-                        return [
-                            // 'onclick' => "window.location.href='{$url}'"
-                            'onclick' => "patientAdmission('{$urlPatientAdmission}'); patientInformation('{$urlPatientInformation}');",
-                            'style' => "cursor:pointer"
-                        ];
-                    },
-                    'columns' => [
-                        ['class' => 'yii\grid\SerialColumn'],
-                            [
-                                'attribute' => 'name',
-                                'headerOptions'=>['style'=>'max-width: 100px;'],
-                                'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
-                                'value' => function($data){
-                                    return  ((new Patient_informationController(null,null)) -> findModel($data->patient_uid))->name;
+                <?php 
+                    if(empty(Yii::$app->request->get('Patient_admissionSearch')))
+                    {
+                        echo Yii::t('app','Patient result record is not found');        
+                    } 
+                    else{
+                        ?>
+                        <!-- This is the gridview that shows patient admission summary-->
+                        <?= kartik\grid\GridView::widget([
+                                'dataProvider' => $dataProvider,
+                            // 'filterModel' => $searchModel,
+                                'showOnEmpty' => false,
+                                'hover' => true,
+                                'striped' => false,
+                                'condensed' => false,
+                                'emptyText' => Yii::t('app','Patient admission record is not founded'),
+                            
+                                'rowOptions' => function($model) {
+                                    // $url = Url::to([Yii::$app->controller->id.'/index', 'id' => $model['patient_uid']]);
+                                    $urlPatientAdmission = Url::toRoute(['patient_admission/patient', 'id' => $model['patient_uid']]);
+                                    $urlPatientInformation = Url::toRoute(['patient_information/patient', 'id' => $model['patient_uid']]);
+                                    return [
+                                        // 'onclick' => "window.location.href='{$url}'"
+                                        'onclick' => "patientAdmission('{$urlPatientAdmission}'); patientInformation('{$urlPatientInformation}');",
+                                        'style' => "cursor:pointer"
+                                    ];
                                 },
-                                'label' => Yii::t('app','Name')
-                            ],
-                            [
-                                'attribute' => 'nric',
-                                'format' => 'raw',
-                                'headerOptions'=>['style'=>'max-width: 100px;'],
-                                'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
-                                'value' => function($data){
-                                    $ic = ((new Patient_informationController(null,null)) -> findModel($data->patient_uid))->nric;
-                                    return  Html::a($ic, \yii\helpers\Url::to(['/site/admission', 'id' => $data['patient_uid'], '#' => 'patient']));
-                                },
-                                'label' => Yii::t('app','NRIC')
-                            ],
-                            [
-                                'attribute' => 'race',
-                                'headerOptions'=>['style'=>'max-width: 100px;'],
-                                'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
-                                'value' => function($data){
-                                    return  ((new Patient_informationController(null,null)) -> findModel($data->patient_uid))->race;
-                                },
-                                'label' => Yii::t('app','Race')
-                            ],
-                            [
-                                'attribute' => 'sex',
-                                'headerOptions'=>['style'=>'max-width: 100px;'],
-                                'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
-                                'value' => function($data){
-                                    return  ((new Patient_informationController(null,null)) -> findModel($data->patient_uid))->sex;
-                                },
-                                'label' => Yii::t('app','Sex')
-                            ],
-                            [
-                                'attribute' => 'rn',
-                                'format' => 'raw',
-                                'headerOptions'=>['style'=>'max-width: 100px;'],
-                                'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
-                                'value'=>function ($data) {
-                                    return Html::a($data['rn'], \yii\helpers\Url::to(['/patient_admission/update', 'rn' => $data['rn']]));
-                                },
-                            ],
-                            [
-                                'attribute' => 'entry_datetime',
-                                "format"=>"raw",
-                                'headerOptions'=>['style'=>'max-width: 100px;'],
-                                'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
-                                'value'=>function ($data) {
-                                    $date = new DateTime($data['entry_datetime']);
-                                    $tag = Html::tag ( 'span' , $date->format('Y-m-d') , [
-                                        // title
-                                        'title' => $date->format('Y-m-d H:i A') ,
-                                        'data-placement' => 'top' ,
-                                        'data-toggle'=>'tooltip',
-                                        'style' => 'white-space:pre;'
-                                    ] );
-                                    return $tag;
-                                },
-                            ],
-                            [
-                                'attribute' => 'billable_sum',
-                                'label' => Yii::t('app','Billable Total').' (RM)',
-                                'headerOptions'=>['style'=>'max-width: 100px;'],
-                                'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
-                                'value' => function($data){
-                                    return  (new Patient_admission()) -> get_billable_sum($data->rn);
-                                },
-                            ],
-                            [
-                                'attribute' => 'amount_due',
-                                'headerOptions'=>['style'=>'max-width: 100px;'],
-                                'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
-                                'value' => function($data){
-                                    return ((new Patient_information())->getBalanceRM($data->patient_uid));
-                                },
-                                'label' => Yii::t('app','Amount Due').' (RM)',
-                            ],
-                            [
-                                'attribute' => 'unclaimed_balance',
-                                'headerOptions'=>['style'=>'max-width: 100px;'],
-                                'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
-                                'value' => function($data){
-                                    return ((new Patient_information())->getUnclaimedBalanceRM($data->patient_uid));
-                                },
-                                'label' => Yii::t('app','Unclaimed Balance').' (RM)',
-                            ],
-                        ],
-                ]) ?>
+                                'columns' => [
+                                    ['class' => 'yii\grid\SerialColumn'],
+                                        [
+                                            'attribute' => 'name',
+                                            'headerOptions'=>['style'=>'max-width: 100px;'],
+                                            'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
+                                            'value' => function($data){
+                                                return  ((new Patient_informationController(null,null)) -> findModel($data->patient_uid))->name;
+                                            },
+                                            'label' => Yii::t('app','Name')
+                                        ],
+                                        [
+                                            'attribute' => 'nric',
+                                            'format' => 'raw',
+                                            'headerOptions'=>['style'=>'max-width: 100px;'],
+                                            'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
+                                            'value' => function($data){
+                                                $ic = ((new Patient_informationController(null,null)) -> findModel($data->patient_uid))->nric;
+                                                return  Html::a($ic, \yii\helpers\Url::to(['/site/admission', 'id' => $data['patient_uid'], '#' => 'patient']));
+                                            },
+                                            'label' => Yii::t('app','NRIC')
+                                        ],
+                                        [
+                                            'attribute' => 'race',
+                                            'headerOptions'=>['style'=>'max-width: 100px;'],
+                                            'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
+                                            'value' => function($data){
+                                                return  ((new Patient_informationController(null,null)) -> findModel($data->patient_uid))->race;
+                                            },
+                                            'label' => Yii::t('app','Race')
+                                        ],
+                                        [
+                                            'attribute' => 'sex',
+                                            'headerOptions'=>['style'=>'max-width: 100px;'],
+                                            'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
+                                            'value' => function($data){
+                                                return  ((new Patient_informationController(null,null)) -> findModel($data->patient_uid))->sex;
+                                            },
+                                            'label' => Yii::t('app','Sex')
+                                        ],
+                                        [
+                                            'attribute' => 'rn',
+                                            'format' => 'raw',
+                                            'headerOptions'=>['style'=>'max-width: 100px;'],
+                                            'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
+                                            'value'=>function ($data) {
+                                                return Html::a($data['rn'], \yii\helpers\Url::to(['/patient_admission/update', 'rn' => $data['rn']]));
+                                            },
+                                        ],
+                                        [
+                                            'attribute' => 'entry_datetime',
+                                            "format"=>"raw",
+                                            'headerOptions'=>['style'=>'max-width: 100px;'],
+                                            'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
+                                            'value'=>function ($data) {
+                                                $date = new DateTime($data['entry_datetime']);
+                                                $tag = Html::tag ( 'span' , $date->format('Y-m-d') , [
+                                                    // title
+                                                    'title' => $date->format('Y-m-d H:i A') ,
+                                                    'data-placement' => 'top' ,
+                                                    'data-toggle'=>'tooltip',
+                                                    'style' => 'white-space:pre;'
+                                                ] );
+                                                return $tag;
+                                            },
+                                        ],
+                                        [
+                                            'attribute' => 'billable_sum',
+                                            'label' => Yii::t('app','Billable Total').' (RM)',
+                                            'headerOptions'=>['style'=>'max-width: 100px;'],
+                                            'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
+                                            'value' => function($data){
+                                                return  (new Patient_admission()) -> get_billable_sum($data->rn);
+                                            },
+                                        ],
+                                        [
+                                            'attribute' => 'amount_due',
+                                            'headerOptions'=>['style'=>'max-width: 100px;'],
+                                            'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
+                                            'value' => function($data){
+                                                return ((new Patient_information())->getBalanceRM($data->patient_uid));
+                                            },
+                                            'label' => Yii::t('app','Amount Due').' (RM)',
+                                        ],
+                                        [
+                                            'attribute' => 'unclaimed_balance',
+                                            'headerOptions'=>['style'=>'max-width: 100px;'],
+                                            'contentOptions'=>['style'=>'max-width: 100px;vertical-align:middle'],
+                                            'value' => function($data){
+                                                return ((new Patient_information())->getUnclaimedBalanceRM($data->patient_uid));
+                                            },
+                                            'label' => Yii::t('app','Unclaimed Balance').' (RM)',
+                                        ],
+                                    ],
+                            ]) ?>
+                    <?php }
+               ?>
         </div>
         <!-- /.card-body -->
     </div>
