3871ed2942cae20b6bfc32fd8b779b4555e072ba
Benjamin Yek <102765903@students.swinburne.edu.my>
2022-08-15 14:02:29 +0800

    Update dbhis.sql

--- dbhis.sql
+++ dbhis.sql
@@ -424,7 +424,7 @@ CREATE TABLE `new_user` (
 INSERT INTO `new_user` (`user_uid`, `username`, `user_password`, `role_cashier`, `role_clerk`, `role_admin`, `role_guest_print`,`Case_Note`,`Registration`,`Charge_Sheet`,`Sticker_Label`, `retire`, `authKey`) VALUES
 ('011BJIjHHpoDWrsDWRyk_dkHc2GUwDBG', 'administrator1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 0, 1, 0,NULL,NULL,NULL,NULL, 0, '12345b'),
 ('2wHPf777EC532SCrMDSR47dTw4nRqx2V', 'cashier1', '7b9efcfad5bc24b82b5acbe6175842f2', 1, 0, 0, 0,NULL,NULL,NULL,NULL, 0, '12345a'),
-('3BUf9deDPpjBuaD7YO3_7vPrmxE4THBo', 'clerk1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 1, 0, 0,NULL,NULL,NULL,NULL, 0, '12345c');
+('3BUf9deDPpjBuaD7YO3_7vPrmxE4THBo', 'clerk1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 1, 0, 0,NULL,NULL,NULL,NULL, 0, '12345c'),
 ('iwJ4pQTEP0chTyfqzfr8KvpSo7XlMQ3S', 'guest_print1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 0, 0, 1,NULL,NULL,NULL,NULL, 0, 'pyLoI1aXGp7sAq72FW-D5u9RxSxub71p');
 
 
