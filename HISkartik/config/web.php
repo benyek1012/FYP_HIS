012d68c2000802c69e61a8949260b0449445ee2b
Benjamin Yek <102765903@students.swinburne.edu.my>
2022-08-15 13:54:56 +0800

    Update web.php

--- HISkartik/config/web.php
+++ HISkartik/config/web.php
@@ -45,7 +45,7 @@ $config = [
         ],
         'formatter' => [
             'thousandSeparator' => ',',
-            'currencyCode' => 'MYR',
+            'currencyCode' => 'RM',
         ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
