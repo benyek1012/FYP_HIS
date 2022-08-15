7b7be1153170ce57b329a7f4f9ba67077782c20c
Benjamin Yek <102765903@students.swinburne.edu.my>
2022-08-15 13:40:10 +0800

    Dashboard for each users

---
+++ HISkartik/views/site/guest_printer_dashboard.php
@@ -0,0 +1,27 @@
+<?php
+    $this->title = Yii::t('app','Guest Printer Dashboard');         
+?>
+
+<body>
+    <div id="card1" class="container-fluid">
+        <div class="card">
+            <div class="card-header text-white bg-primary">
+                <h3 class="card-title"><?php echo Yii::t('app','Nothing');?></h3>
+                <div class="card-tools">
+                    <!-- Collapse Button -->
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i
+                            class="fas fa-minus"></i></button>
+                </div>
+                <!-- /.card-tools -->
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <!-- This is the form that shows patient information which can directly updating-->
+             
+             
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+</body>
