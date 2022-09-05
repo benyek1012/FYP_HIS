d50d31e4dbd7437933b6de3ac56f69d79044028a
Benjamin Yek <102765903@students.swinburne.edu.my>
2022-09-05 13:35:27 +0800

    Create update_script.sql

---
+++ update_script.sql
@@ -0,0 +1,5 @@
+/* Update Error change to varchar(500000) Version 0.2.5 */
+
+ALTER TABLE `pekeliling_import` CHANGE `error` `error` VARCHAR(500000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
+
+/* Update Error change to varchar(500000) Version 0.2.5 END */
