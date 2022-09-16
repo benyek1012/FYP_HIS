20e7f35cc918041b8bfa5842c64d1e28be2f375b
Benjamin Yek <102765903@students.swinburne.edu.my>
2022-09-16 22:07:25 +0800

    Update update_script.sql

--- update_script.sql
+++ update_script.sql
@@ -29,28 +29,26 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `reminder_select_number`(IN `batch_d
 BEGIN
 
 SELECT rn, nric, race, address1, address2, address3, guarantor_nric, guarantor_address, entry_datetime, reminder_no AS 'Reminder Number', reminder_batch_date AS 'Batch date', final_ward_datetime AS 'Discharge Date', bill_generation_billable_sum_rm AS 'Billable Fee'
-FROM ( 
-SELECT bill.rn, patient_information.nric, patient_information.race, patient_information.address1, patient_information.address2, patient_information.address3, patient_admission.guarantor_nric, patient_admission.guarantor_address, patient_admission.entry_datetime, 'reminder1' reminder_no, reminder1 reminder_batch_date, bill.final_ward_datetime, bill.deleted, receipt.receipt_type, receipt.receipt_content_sum, receipt.receipt_uid, receipt.receipt_content_datetime_paid, bill.bill_generation_billable_sum_rm
+FROM (
+SELECT bill.rn, patient_information.nric, patient_information.race, patient_information.address1, patient_information.address2, patient_information.address3, patient_admission.guarantor_nric, patient_admission.guarantor_address, patient_admission.entry_datetime, 'reminder1' reminder_no, reminder1 reminder_batch_date, bill.final_ward_datetime, bill.deleted, bill.bill_generation_billable_sum_rm
     FROM patient_admission 
 	LEFT JOIN patient_information ON patient_admission.patient_uid = patient_information.patient_uid
 	LEFT JOIN bill ON patient_admission.rn = bill.rn
- 	LEFT JOIN receipt ON patient_admission.rn = receipt.rn
-    
+    WHERE bill.bill_generation_datetime is not null
     union all 
     
-SELECT bill.rn, patient_information.nric, patient_information.race, patient_information.address1, patient_information.address2, patient_information.address3, patient_admission.guarantor_nric, patient_admission.guarantor_address, patient_admission.entry_datetime, 'reminder2' reminder_no, reminder2 reminder_batch_date, bill.final_ward_datetime, bill.deleted, receipt.receipt_type, receipt.receipt_content_sum, receipt.receipt_uid, receipt.receipt_content_datetime_paid, bill.bill_generation_billable_sum_rm
+SELECT bill.rn, patient_information.nric, patient_information.race, patient_information.address1, patient_information.address2, patient_information.address3, patient_admission.guarantor_nric, patient_admission.guarantor_address, patient_admission.entry_datetime, 'reminder2' reminder_no, reminder2 reminder_batch_date, bill.final_ward_datetime, bill.deleted, bill.bill_generation_billable_sum_rm
     FROM patient_admission
 	LEFT JOIN patient_information ON patient_admission.patient_uid = patient_information.patient_uid
 	LEFT JOIN bill ON patient_admission.rn = bill.rn
- 	LEFT JOIN receipt ON patient_admission.rn = receipt.rn
-    
+    WHERE bill.bill_generation_datetime is not null
 	union all 
     
-SELECT bill.rn, patient_information.nric, patient_information.race, patient_information.address1, patient_information.address2, patient_information.address3, patient_admission.guarantor_nric, patient_admission.guarantor_address, patient_admission.entry_datetime, 'reminder3' reminder_no, reminder3 reminder_batch_date, bill.final_ward_datetime, bill.deleted, receipt.receipt_type, receipt.receipt_content_sum, receipt.receipt_uid, receipt.receipt_content_datetime_paid, bill.bill_generation_billable_sum_rm
+SELECT bill.rn, patient_information.nric, patient_information.race, patient_information.address1, patient_information.address2, patient_information.address3, patient_admission.guarantor_nric, patient_admission.guarantor_address, patient_admission.entry_datetime, 'reminder3' reminder_no, reminder3 reminder_batch_date, bill.final_ward_datetime, bill.deleted, bill.bill_generation_billable_sum_rm
     FROM patient_admission
 	LEFT JOIN patient_information ON patient_admission.patient_uid = patient_information.patient_uid
 	LEFT JOIN bill ON patient_admission.rn = bill.rn
-	LEFT JOIN receipt ON patient_admission.rn = receipt.rn
+    WHERE bill.bill_generation_datetime is not null
 )  dummy_name
 WHERE reminder_batch_date is not null AND deleted = 0 AND reminder_batch_date <= batch_date 
 ORDER BY rn, reminder_no;
