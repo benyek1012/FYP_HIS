41d0254854976a32f0d644098c67bd3b2bde0cf3
clarence <102765877@students.swinburne.edu.my>
2022-09-20 23:49:46 +0800

    Update update_script.sql

--- update_script.sql
+++ update_script.sql
@@ -58,5 +58,32 @@ DELIMITER ;
 
 ALTER TABLE `variable` ADD `hospital_name` VARCHAR(64) NOT NULL DEFAULT 'Sarawak General Hospital' AFTER `read_only`, ADD `director_name` VARCHAR(64) NOT NULL AFTER `hospital_name`;
 	
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `transaction_records`(IN `pid` VARCHAR(64))
+BEGIN
+
+	SELECT receipt.rn, receipt.receipt_content_datetime_paid, receipt.receipt_content_sum, receipt.receipt_type, 	receipt.receipt_content_payment_method, receipt.receipt_content_payer_name, receipt.receipt_serial_number, receipt.receipt_content_description, receipt.receipt_responsible
+	FROM receipt
+	INNER JOIN patient_admission 
+    ON patient_admission.rn = receipt.rn
+    INNER JOIN patient_information
+    ON patient_information.patient_uid = patient_admission.patient_uid
+    WHERE patient_information.patient_uid = pid
+    
+    UNION
+     
+    SELECT bill.rn, bill.bill_generation_datetime, bill.bill_generation_billable_sum_rm, null, null, null, bill.bill_print_id, null, bill.generation_responsible_uid 
+    FROM bill 
+    INNER JOIN patient_admission 
+    ON patient_admission.rn = bill.rn
+    INNER JOIN patient_information
+    ON patient_information.patient_uid = patient_admission.patient_uid
+    WHERE patient_information.patient_uid = pid 
+    AND bill.bill_generation_datetime is not null;
+    
+ 
+END$$
+DELIMITER ;
+
 
 /* Update Error change to varchar(500000) Version 0.2.5 END */
