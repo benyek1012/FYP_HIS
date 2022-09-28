e4e5d9e13b4f744aad917141b2fb02a82ac35239
thelaw29 <100083674@students.swinburne.edu.my>
2022-09-28 22:58:38 +0800

    User Roles Gridview update
    - entry form & db & rules: Change so active = 1 by default (so active = 1 means active, active = 0 means inactive)
    - main sql file updated so that update 0.2.6 will not be required when database breaks. for current update use update_script 0.2.6 to update new_user table otherwise all accounts could not login into the website

--- sghis.sql
+++ sghis.sql
@@ -6449,7 +6449,7 @@ CREATE TABLE `new_user` (
   `Registration` varchar(64) DEFAULT NULL,
   `Charge_Sheet` varchar(64) DEFAULT NULL,
   `Sticker_Label` varchar(64) DEFAULT NULL,
-  `retire` tinyint(1) NOT NULL DEFAULT 0,
+  `retire` tinyint(1) NOT NULL DEFAULT 1,
   `authKey` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -6458,10 +6458,10 @@ CREATE TABLE `new_user` (
 --
 
 INSERT INTO `new_user` (`user_uid`, `username`, `user_password`, `role_cashier`, `role_clerk`, `role_admin`, `role_guest_print`, `Case_Note`, `Registration`, `Charge_Sheet`, `Sticker_Label`, `retire`, `authKey`) VALUES
-('011BJIjHHpoDWrsDWRyk_dkHc2GUwDBG', 'administrator1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 0, 1, 0, NULL, NULL, NULL, NULL, 0, '12345b'),
-('2wHPf777EC532SCrMDSR47dTw4nRqx2V', 'cashier1', '7b9efcfad5bc24b82b5acbe6175842f2', 1, 0, 0, 0, NULL, NULL, NULL, NULL, 0, '12345a'),
-('3BUf9deDPpjBuaD7YO3_7vPrmxE4THBo', 'clerk1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 1, 0, 0, NULL, NULL, NULL, NULL, 0, '12345c'),
-('iwJ4pQTEP0chTyfqzfr8KvpSo7XlMQ3S', 'guest_print1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 0, 0, 1, NULL, NULL, NULL, NULL, 0, 'pyLoI1aXGp7sAq72FW-D5u9RxSxub71p');
+('011BJIjHHpoDWrsDWRyk_dkHc2GUwDBG', 'administrator1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 0, 1, 0, NULL, NULL, NULL, NULL, 1, '12345b'),
+('2wHPf777EC532SCrMDSR47dTw4nRqx2V', 'cashier1', '7b9efcfad5bc24b82b5acbe6175842f2', 1, 0, 0, 0, NULL, NULL, NULL, NULL, 1, '12345a'),
+('3BUf9deDPpjBuaD7YO3_7vPrmxE4THBo', 'clerk1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 1, 0, 0, NULL, NULL, NULL, NULL, 1, '12345c'),
+('iwJ4pQTEP0chTyfqzfr8KvpSo7XlMQ3S', 'guest_print1', '7b9efcfad5bc24b82b5acbe6175842f2', 0, 0, 0, 1, NULL, NULL, NULL, NULL, 1, 'pyLoI1aXGp7sAq72FW-D5u9RxSxub71p');
 
 -- --------------------------------------------------------
 
