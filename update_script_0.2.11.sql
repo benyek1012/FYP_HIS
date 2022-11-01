aee7dc8a2c54d3063f6638740997f4d9921b4e17
clarence <102765877@students.swinburne.edu.my>
2022-11-01 22:37:56 +0800

    Update update_script_0.2.11.sql

--- update_script_0.2.11.sql
+++ update_script_0.2.11.sql
@@ -266,3 +266,34 @@ CREATE TABLE `inpatient_treatment`( `inpatient_treatment_uid` VARCHAR(64) NOT NU
 ALTER TABLE `inpatient_treatment` ADD PRIMARY KEY(`inpatient_treatment_uid`);
 
 ALTER TABLE `inpatient_treatment` ADD INDEX(`bill_uid`);
+
+DROP PROCEDURE IF EXISTS `transaction_records`;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `transaction_records`(IN `pid` VARCHAR(64), IN `rn` VARCHAR(64))
+BEGIN
+
+	SELECT receipt.rn, receipt.receipt_content_datetime_paid, receipt.receipt_content_sum, receipt.receipt_type, 	receipt.receipt_content_payment_method, receipt.receipt_content_payer_name, receipt.receipt_serial_number, receipt.receipt_content_description, receipt.receipt_responsible,  receipt.receipt_uid
+	FROM receipt
+	INNER JOIN patient_admission 
+    ON patient_admission.rn = receipt.rn
+    INNER JOIN patient_information
+    ON patient_information.patient_uid = patient_admission.patient_uid
+    WHERE patient_information.patient_uid = pid
+    AND receipt.rn = rn
+    
+    UNION
+     
+    SELECT bill.rn, bill.bill_generation_datetime, bill.bill_generation_billable_sum_rm, null, null, null, bill.bill_print_id, null, bill.generation_responsible_uid, null
+    FROM bill 
+    INNER JOIN patient_admission 
+    ON patient_admission.rn = bill.rn
+    INNER JOIN patient_information
+    ON patient_information.patient_uid = patient_admission.patient_uid
+    WHERE patient_information.patient_uid = pid 
+    AND bill.bill_generation_datetime is not null
+    AND bill.rn = rn;
+    
+ 
+END$$
+DELIMITER ;
