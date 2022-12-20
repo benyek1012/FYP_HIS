8bdb73d30e7acba6d42c547973dc2a83a318d7c1
xueqian666 <zhangxueqian666@gmail.com>
2022-12-20 21:06:34 +0800

    Update_script_0.2.14.sql

--- update_script_0.2.14.sql
+++ update_script_0.2.14.sql
@@ -118,3 +118,6 @@ INSERT INTO `patient_information` (`patient_uid`, `first_reg_date`, `nric`, `nat
 
 INSERT INTO `patient_admission` (`rn`, `entry_datetime`, `patient_uid`, `initial_ward_code`, `initial_ward_class`, `reference`, `medical_legal_code`, `type`, `reminder1`, `reminder2`, `reminder3`) VALUES
 ('LAINLAIN', '1950-01-01 00:00:00', 'm-vPBL8igioXLRSnMSBsF6awk4jOgM0C', '01', '3', 'Default', 0, 'Normal', NULL, NULL, NULL);('LAINLAIN', '1950-01-01 00:00:00', 'm-vPBL8igioXLRSnMSBsF6awk4jOgM0C', '01', '3', 'Default', 0, 'Normal', NULL, NULL, NULL);
+
+
+ALTER TABLE `bill` ADD `bill_forgive_date` DATETIME NULL DEFAULT NULL AFTER `discharge_date`;
