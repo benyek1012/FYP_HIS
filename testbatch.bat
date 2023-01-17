273875fe42c0e52090d1e448cc957a91c9c6f518
thelaw29 <100083674@students.swinburne.edu.my>
2023-01-18 01:21:20 +0800

    Update testbatch.bat
    Set time format
    
    CUR_YYYY is year
    CUR_MM is Month
    CUR_DD is Day
    CUR_HH is hour
    CUR_NN is minutes
    CUR_SS is seconds

--- testbatch.bat
+++ testbatch.bat
@@ -1,7 +1,18 @@
 @echo off
 
-set TIMESTAMP=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%_%TIME:~0,2%_%TIME:~3,2%
+set CUR_YYYY=%date:~10,4%
+set CUR_MM=%date:~4,2%
+set CUR_DD=%date:~7,2%
+set CUR_HH=%time:~0,2%
+if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
 
-"c:\xampp\mysql\bin\mysqldump.exe" -uSGHISadmin -p!9aj59hQX5l1SU5n -hlocalhost SGHIS> C:\xampp\mysql\bin\full_backup.sql
+set CUR_NN=%time:~3,2%
+set CUR_SS=%time:~6,2%
+
+set SUBFILENAME=%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
+
+"c:\xampp\mysql\bin\mysqldump.exe" -uSGHISadmin -p!9aj59hQX5l1SU5n -hlocalhost SGHIS > D:\DB\SGHIS_FULLBACKUP_%SUBFILENAME%.sql
+"c:\xampp\mysql\bin\mysqldump.exe" -uSGHISadmin -p!9aj59hQX5l1SU5n -hlocalhost SGHIS > C:\Users\joshl\Desktop\DB\SGHIS_FULLBACKUP_%SUBFILENAME%.sql
 echo Done!
 pause
+exit
